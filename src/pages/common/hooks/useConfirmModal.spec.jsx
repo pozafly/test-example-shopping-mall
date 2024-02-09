d126f926f46cba8e1aeb2582457ffae0e0af3513
pozafly <pozafly@gmail.com>
2024-02-09 20:47:36 +0900

    hooks test

--- src/pages/common/hooks/useConfirmModal.spec.jsx
+++ src/pages/common/hooks/useConfirmModal.spec.jsx
@@ -2,8 +2,25 @@ import { renderHook, act } from '@testing-library/react';
 
 import useConfirmModal from './useConfirmModal';
 
-it('호출 시 initialValue 인자를 지정하지 않는 경우 isModalOpened 상태가 false로 설정된다.', () => {});
+// 리액트 훅은 반드시 리액트 컴포넌트 내에서만 호출되어야 정상적으로 실행
+it('호출 시 initialValue 인자를 지정하지 않는 경우 isModalOpened 상태가 false로 설정된다.', () => {
+  // result: 훅을 호출해 얻은 결과 값을 반환 -> result.current 값의 참조를 통해 최신 상태를 추적할 수 있다.
+  // rerender: 훅을 원하는 인자와 함께 새로 호출해 상태를 갱신한다.
+  const { result, rerender } = renderHook(useConfirmModal);
+  expect(result.current.isModalOpened).toBe(false);
+});
 
-it('호출 시 initialValue 인자를 boolean 값으로 지정하는 경우 해당 값으로 isModalOpened 상태가 설정된다.', () => {});
+it('호출 시 initialValue 인자를 boolean 값으로 지정하는 경우 해당 값으로 isModalOpened 상태가 설정된다.', () => {
+  const { result, rerender } = renderHook(() => useConfirmModal(true));
+  expect(result.current.isModalOpened).toBe(true);
+});
 
-it('훅의 toggleIsModalOpened()를 호출하면 isModalOpened 상태가 toggle된다.', () => {});
+it('훅의 toggleIsModalOpened()를 호출하면 isModalOpened 상태가 toggle된다.', () => {
+  const { result, rerender } = renderHook(useConfirmModal);
+
+  act(() => {
+    result.current.toggleIsModalOpened();
+  });
+
+  expect(result.current.isModalOpened).toBe(true);
+});
